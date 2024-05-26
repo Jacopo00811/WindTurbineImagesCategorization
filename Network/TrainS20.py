7ba91401fdd89734c044dddbb914466ccfb2f4be
Jacopo00811 <jacopo.ceccuti@gmail.com>
2024-05-26 19:36:35 +0200

    fixed values for top 2 run

--- Network/TrainS20.py
+++ Network/TrainS20.py
@@ -194,8 +194,8 @@ def train_net(model, loss_function, device, dataloader_train, dataloader_validat
 
 MEAN = np.array([0.5750, 0.6065, 0.6459])
 STD = np.array([0.1854, 0.1748, 0.1794])
-ROOT_DIRECTORY = "c:\\Users\\jacop\\Desktop\\BSc\\Code\\WindTurbineImagesCategorization\\Data\\DatasetPNG"
-# ROOT_DIRECTORY = "/zhome/f9/0/168881/Desktop/WindTurbineImagesCategorization/Data/DatasetPNG"
+# ROOT_DIRECTORY = "c:\\Users\\jacop\\Desktop\\BSc\\Code\\WindTurbineImagesCategorization\\Data\\DatasetPNG"
+ROOT_DIRECTORY = "/zhome/f9/0/168881/Desktop/WindTurbineImagesCategorization/Data/DatasetPNG"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Device in use: {DEVICE}")
 
@@ -219,10 +219,10 @@ hyper_parameters = {
     "input channels": 3,
     "number of classes": 5,
     "split": {"train": 0.6, "val": 0.2, "test": 0.2},
-    "batch size": 32, # 64
+    "batch size": 64,
     "number of workers": 0,
     "learning rate": 0.001,
-    "epochs": 1, #270
+    "epochs": 270,
     "beta1": 0.9,
     "beta2": 0.999,
     "epsilon": 1e-08,
